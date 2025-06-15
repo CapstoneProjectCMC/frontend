d4a065a52cc84654c077869b0c7c52b57d2313c9
Fxdonad <toquangduc2004@gmail.com>
2025-06-15 14:59:34 +0700

    replace some code position

--- src/app/features/dashboard/pages/statistics/statistics.ts
+++ src/app/features/dashboard/pages/statistics/statistics.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NotificationTestComponent } from '../../../../shared/components/fxdonad-shared/notification-test/notification-test';
+import { Tooltip } from '../../../../shared/components/fxdonad-shared/tooltip/tooltip';
+import { CodeEditorComponent } from '../../../../shared/components/fxdonad-shared/code-editor/code-editor.component';
 
 @Component({
   selector: 'app-statistics',
-  imports: [],
+  imports: [NotificationTestComponent, CodeEditorComponent],
   templateUrl: './statistics.html',
   styleUrl: './statistics.scss',
 })
