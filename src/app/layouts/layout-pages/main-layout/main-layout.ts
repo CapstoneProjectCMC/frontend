1dbbfdc85577c80aecd5548b7970beb9e14a5895
nguyentramy1 <ntramy1010@gmail.com>
2025-07-10 14:46:12 +0700

    responsive for header and footer

--- src/app/layouts/layout-pages/main-layout/main-layout.ts
+++ src/app/layouts/layout-pages/main-layout/main-layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../../../shared/components/my-shared/header/header';
+import { FooterComponent } from '../../../shared/components/my-shared/footer/footer';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
