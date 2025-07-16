9b216570ba1169320ed803318c86a49bbdc131d9
Tô Quang Đức <Toquangduc2004@gmail.com>
2025-07-16 22:36:46 +0700

    add exercise details page base

--- src/app/core/models/exercise.model.ts
+++ src/app/core/models/exercise.model.ts
@@ -11,3 +11,80 @@ export type ExerciseItem = {
   tags: Set<string>;
   createdAt: string;
 };
+
+//Quiz
+// Interface cho option của câu hỏi
+export interface QuizOption {
+  id: string;
+  optionText: string;
+  correct: boolean;
+  order: string;
+  createdBy: string;
+  createdAt: string;
+  updatedBy: string;
+  updatedAt: string;
+  deletedBy: string;
+  deletedAt: string;
+}
+
+// Interface cho câu hỏi
+export interface QuizQuestion {
+  id: string;
+  text: string;
+  points: number;
+  type: string;
+  orderInQuiz: number;
+  options: QuizOption[];
+  createdBy: string;
+  createdAt: string;
+  updatedBy: string;
+  updatedAt: string;
+  deletedBy: string;
+  deletedAt: string;
+}
+
+// Interface cho quizDetail
+export interface QuizDetail {
+  id: string;
+  numQuestions: number;
+  totalPoints: number;
+  currentPage: number;
+  totalPages: number;
+  pageSize: number;
+  totalElements: number;
+  questions: QuizQuestion[];
+  createdBy: string;
+  createdAt: string;
+  updatedBy: string;
+  updatedAt: string;
+  deletedBy: string;
+  deletedAt: string;
+}
+
+// Interface cho bài tập
+export interface ExerciseQuiz {
+  id: string;
+  userId: string;
+  title: string;
+  description: string;
+  exerciseType: string;
+  difficulty: 'EASY' | 'MEDIUM' | 'HARD';
+  orgId: string;
+  active: boolean;
+  cost: number;
+  freeForOrg: boolean;
+  startTime: string;
+  endTime: string;
+  duration: number;
+  allowDiscussionId: string;
+  resourceIds: string[];
+  tags: string[];
+  allowAiQuestion: boolean;
+  quizDetail: QuizDetail;
+  createdBy: string;
+  createdAt: string;
+  updatedBy: string;
+  updatedAt: string;
+  deletedBy: string;
+  deletedAt: string;
+}
--- src/app/features/excercise/exercise-routing.module.ts
+++ src/app/features/excercise/exercise-routing.module.ts
@@ -2,12 +2,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListExerciseComponent } from './list-exercise/list-exercise.component';
+import { ExerciseDetailsComponent } from './exercise-details/exercise-details.component';
 
 const routes: Routes = [
   {
     path: 'exercise-list',
     component: ListExerciseComponent,
     data: { breadcrumb: 'Danh sách bài tập' },
+  },
+  {
+    path: 'exercise-list/exercise-details/:id',
+    component: ExerciseDetailsComponent,
+    data: { breadcrumb: 'Chi tiết bài tập' },
   },
 ];
 
