7c2ad4ccb15d635a2a16ba9309d79e8f9786a83b
Tô Quang Đức <Toquangduc2004@gmail.com>
2025-09-03 23:12:58 +0700

    add config ip

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  IP_SERVER: 'http://localhost:8888/api',
+  // IP_SERVER: 'http://192.168.1.220:8888/api',
+  IP_SERVER_NO_SSL: 'http://192.168.1.220:8000/api',
+  IP_SERVER_RADMIN: 'http://26.100.147.137:8888/api',
+  IP_LOCAL: 'http://localhost:8081/api',
+};
+
+//Client ID: 839020123858-qnan968uvj0u9d5h6bq5cd5ulls9h7dk.apps.googleusercontent.com
+
+//client Secret: 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
