6bc3b20633e00a2cbb77c57dc0aa5655e61c75bc
Tô Quang Đức <Toquangduc2004@gmail.com>
2025-09-03 23:00:40 +0700

    fix warning

--- src/app/shared/components/fxdonad-shared/skeleton-loading/skeleton-loading.component.ts
+++ src/app/shared/components/fxdonad-shared/skeleton-loading/skeleton-loading.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 
-
 @Component({
   selector: 'app-skeleton-loading',
   standalone: true,
@@ -11,4 +10,8 @@ import { Component, Input } from '@angular/core';
 export class SkeletonLoadingComponent {
   @Input() type: 'card' | 'list' | 'post' = 'card';
   @Input() count: number = 1;
+
+  get skeletonArray() {
+    return Array.from({ length: this.count }, (_, i) => i);
+  }
 }
