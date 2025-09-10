d3e7e7c07f2112e1d50c227ca6d7d98c66149937
Yunom Yxvia <yunomix2834@gmail.com>
2025-09-10 22:08:26 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,4 +4,5 @@ export const environment = {
   IP_SERVER_NO_SSL: 'http://192.168.1.220:8000/api',
   IP_SERVER_RADMIN: 'http://26.100.147.137:8888/api',
   IP_LOCAL: 'http://localhost:8081/api',
-}; 
+};
+ 
