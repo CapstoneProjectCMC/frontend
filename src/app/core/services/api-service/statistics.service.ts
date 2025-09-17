c7c97d7eac4dbf782a4f0a2ac7c5588d285204ed
nguyentramy1 <ntramy1010@gmail.com>
2025-09-17 15:08:05 +0700

    user-payment-statistic, payment-statistic

--- src/app/core/services/api-service/statistics.service.ts
+++ src/app/core/services/api-service/statistics.service.ts
@@ -3,6 +3,8 @@ import { ApiMethod } from '../config-service/api.methods';
 import { ApiResponse, IPaginationResponse } from '../../models/api-response';
 import {
   ExerciseStatisticsResponse,
+  PaymentStatisticsAdmin,
+  PaymentStatisticsUser,
   SummaryStatisticsAdmin,
 } from '../../models/statistics.model';
 import { API_CONFIG } from '../config-service/api.enpoints';
@@ -22,6 +24,16 @@ export class StatisticsService {
   getAdminSummary() {
     return this.api.get<ApiResponse<SummaryStatisticsAdmin>>(
       API_CONFIG.ENDPOINTS.GET.GET_SUMMARY_STATISTICS_ADMIN
+    );
+  }
+  getAdminPaymentStats(year: number, month: number) {
+    return this.api.get<ApiResponse<PaymentStatisticsAdmin[]>>(
+      API_CONFIG.ENDPOINTS.GET.GET_PAYMENT_STATISTICS_ADMIN(year, month)
+    );
+  }
+  getUserPaymentStats(year: number, month: number) {
+    return this.api.get<ApiResponse<PaymentStatisticsUser[]>>(
+      API_CONFIG.ENDPOINTS.GET.GET_USER_PAYMENT_STATISTICS_ADMIN(year, month)
     );
   }
 }
--- src/app/features/statistics/statistics-routing.module.ts
+++ src/app/features/statistics/statistics-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { StatisticsLayoutComponent } from './statistics-layout/statistics-layout.component';
 import { ExerciseAdminStatisticsComponent } from './pages/exercise-admin-statistics/exercise-admin-statistics.component';
 import { SummaryStatisticsComponent } from './pages/summary-statistics/summary-statistics.component';
+import { PaymentStatisticsComponent } from './pages/payment-statistics/payment-statistics.component';
+import { UserPaymentStatisticsComponent } from './pages/user-payment-statistic/user-payment-statistic.component';
 
 const routes: Routes = [
   {
@@ -19,6 +21,16 @@ const routes: Routes = [
         path: 'admin-chart-exercise-statistics',
         component: SummaryStatisticsComponent,
         data: { breadcrumb: 'Thống kê bài tập' },
+      },
+      {
+        path: 'admin-payment-statistics', //thêm vào đây
+        component: PaymentStatisticsComponent, //thêm vào đây
+        data: { breadcrumb: 'Thống kê doanh thu' },
+      },
+      {
+        path: 'user-payment-statistics', //thêm vào đây
+        component: UserPaymentStatisticsComponent, //thêm vào đây
+        data: { breadcrumb: 'Thống kê nạp & mua' },
       },
       //thêm vào đây
     ],
